e3ae4a048152d0f73049255f2345bb130f4ce312
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2024-08-05 17:50:14 -0300

    Fix katex support

--- Material/docusaurus.config.ts
+++ Material/docusaurus.config.ts
@@ -70,12 +70,9 @@ const config: Config = {
     },
   stylesheets: [
       {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      href: '/katex/katex.min.css',
       type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
+      },
   ],
   } satisfies Preset.ThemeConfig,
 };
